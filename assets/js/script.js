1f89c6a7116c1eed8d3ceb83f135bf27be3faa04
KevinPolanco <polancockp@gmail.com>
2021-09-30 11:59:24 -0300

    agregue animaciones a secciones de quienes somos y destacados con AOS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
-// codigo que cambia de color la barra de navegacion al hacer scroll
 
-var nav = document.querySelector("nav");
+$(function(){
+    var nav = document.querySelector("nav");
 
-window.addEventListener("scroll", function(){
-    if(window.pageYOffset > 100){
-        nav.classList.add("celeste","shadow");
-    }else{
-        nav.classList.remove("celeste", "shadow");
-    }
+    // codigo que cambia de color la barra de navegacion al hacer scroll
+    window.addEventListener("scroll", function(){
+        if(window.pageYOffset > 100){
+            nav.classList.add("celeste","shadow");
+        }else{
+            nav.classList.remove("celeste", "shadow");
+        }
+    });
+
+
+   
+
 });
